512d75cd57672dad8d248f94b5eebea77368da82
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-17 12:44:36 +1000

    улучшил читаемость метода Normalize

--- normalizer.go
+++ normalizer.go
@@ -4,9 +4,11 @@ func Normalize(snils string) string {
 	runes := make([]rune, 0, len(snils))
 
 	for _, r := range snils {
-		if !isSpace(r) && !isDelimiter(r) {
-			runes = append(runes, r)
+		if isSpace(r) || isDelimiter(r) {
+			continue
 		}
+
+		runes = append(runes, r)
 	}
 
 	return string(runes)
