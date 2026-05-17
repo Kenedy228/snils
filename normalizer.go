91ab4c0458b2378872183d8529f0b96fb8ce7b63
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-17 12:30:07 +1000

    реализовал функцию Normalize

--- normalizer.go
+++ normalizer.go
@@ -1,5 +1,13 @@
 package snils
 
 func Normalize(snils string) string {
+	runes := make([]rune, 0, len(snils))
 
+	for _, r := range snils {
+		if !isSpace(r) && !isDelimiter(r) {
+			runes = append(runes, r)
+		}
+	}
+
+	return string(runes)
 }
